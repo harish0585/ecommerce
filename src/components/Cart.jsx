3ae539fa1e5c59ead05c9151d0a131f95d19d035
harish0585 <harish.1994.19@gmail.com>
2022-10-28 12:48:12 +0530

    New Changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>Welcome to Cart Page</div>
   )
 }
 
--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Orders = () => {
   return (
     <div>
-      
+      Welcome to Orders Page
     </div>
   )
 }
